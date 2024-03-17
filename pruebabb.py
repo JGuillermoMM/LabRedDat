0c97608e707e8caed95eecbb66d11a7fee2c00c9
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-17 14:52:40 -0600

    Update pruebabb.py

--- pruebabb.py
+++ pruebabb.py
@@ -11,133 +11,80 @@ import json
 
 from streamlit_lottie import st_lottie
 
-
-
-#Layout
+# Layout
 st.set_page_config(
     page_title="Practica 1",
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="expanded"
+)
 
-#Data Pull and Functions
-st.markdown("""
-<style>
-.big-font {
-    font-size:80px !important;
-}
-</style>
-""", unsafe_allow_html=True)
+# Data Pull and Functions
 
-@st.cache_data
-def load_lottiefile(filepath: str):
-    with open(filepath,"r") as f:
-        return json.load(f)
-
-@st.cache_data
-def pull_clean():
-    master_zip=pd.read_csv('ConteoDeCarasPorParejas.csv',dtype={'ZCTA5': str})
-    return master_zip
-
-
-
-#Options Menu
+# Options Menu
 with st.sidebar:
-    selected = option_menu('Menu', ["Intro", 'Graficas','Info', 'Distribucion binomial'], 
-        icons=['comment-alt','stats','info-circle'],menu_icon='intersect', default_index=0)
-    lottie = load_lottiefile("digglet.json")
-    st_lottie(lottie,key='loc')
+    selected = st.radio('Menu', ["Intro", 'Graficas','Info', 'Distribucion binomial'], index=0)
 
-#Intro Page
-if selected=="Intro":
-    #Header
+# Intro Page
+if selected == "Intro":
+    # Header
     st.title('Distribución Binomial')
     st.subheader('*asdasdassssssssssssssssssssssssssssssssssss.*')
-
     st.divider()
-
-    #Use Cases
+    # Use Cases
     with st.container():
-        col1,col2=st.columns(2)
+        col1, col2 = st.columns(2)
         with col1:
             st.header('Resumen')
-            st.markdown(
-                """
+            st.markdown("""
                 asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-                """
-                )
+            """)
             st.header('Objetivos')
-            st.markdown(
-                """
+            st.markdown("""
                 asdddddddddddddddddddd
-                """
-                )
-            
+            """)
         with col2:
             st.header('Marco teorico')
-            st.markdown(
-                """
+            st.markdown("""
                 asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                """
-                )
+            """)
             st.header('Conclusiones')
-            st.markdown(
-                """
+            st.markdown("""
                 asdddddddddddddddddddd
-                """)
-
+            """)
     
+# Search Page
+elif selected == 'Graficas':
+    # Add a title to the app
+    st.title("CSV File Viewer")
+    # Add a file uploader to the app
+    df = pd.read_csv("ConteosDeCarasPorPareja.csv")
+    # Display the DataFrame in Streamlit
+    st.dataframe(df)
+    # Display some statistics about the DataFrame
+    st.write(f"The DataFrame has {len(df)} rows and {len(df.columns)} columns.")
 
-
-    
-#Search Page
-if selected=='Graficas':
-    # Set up the Streamlit ap
-
- # Add a title to the app
- st.title("CSV File Viewer")
-
-# Add a file uploader to the app
- 
-
- df = pd.read_csv("ConteosDeCarasPorPareja.csv")
-  # Display the DataFrame in Streamlit
- st.dataframe(df)
-
-   # Display some statistics about the DataFrame
- st.write(f"The DataFrame has {len(df)} rows and {len(df.columns)} columns.")
-
-
-
-#---------------------------------------------------------------
 # Grafica
-if selected=='Distribucion binomial':
+elif selected == 'Distribucion binomial':
+    def leer_archivo_py():
+        try:
+            with open("Practica1.py", "r") as archivo:
+                contenido = archivo.read()
+                st.text_area("Contenido del archivo", contenido)
+        except FileNotFoundError:
+            st.error("El archivo no fue encontrado.")
 
-def leer_archivo_py():
-    try:
-        with open("Practica1.py", "r") as archivo:
-            contenido = archivo.read()
-            st.text_area("Contenido del archivo", contenido)
-    except FileNotFoundError:
-        st.error("El archivo no fue encontrado.")
+    def main():
+        st.title("aaaaa")
+        st.write("sdasdasdasd.")
 
-def main():
-    st.title("aaaaa")
-    st.write("sdasdasdasd.")
+        leer_archivo_py()
 
-    leer_archivo_py()
+    if __name__ == "__main__":
+        main()
 
-#-----------------------------------------------------------------
-    
-  
-
- 
-#About Page
-if selected=='Info': 
+# About Page
+elif selected == 'Info': 
     st.title('Datos')
-    st.markdown(
-                """
-                asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                """)
-
-if __name__ == "__main__":
-    main()
+    st.markdown("""
+        asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    """)
