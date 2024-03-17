c45a58b5736538897822199473706e4d8e4f356a
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-17 16:32:59 -0600

    Update pruebabb.py

--- pruebabb.py
+++ pruebabb.py
@@ -50,7 +50,11 @@ with st.sidebar:
 if selected=="Intro":
     #Header
     st.title('Distribución Binomial')
-    st.subheader('*asdasdassssssssssssssssssssssssssssssssssss.*')
+    st.subheader('*   # ecuacion de la distribucion binomial "https://es.wikipedia.org/wiki/Distribuci%C3%B3n_binomial"
+    st.latex(r'''
+    P(X = k) = \binom{n}{k} \cdot p^k \cdot (1 - p)^{n - k}
+    ''')*')
+                 
 
     st.divider()
 
