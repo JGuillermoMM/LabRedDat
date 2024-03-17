d30a2aaed585477648fb77344761a4a858dc557c
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-17 15:09:03 -0600

    Update pruebabb.py

--- pruebabb.py
+++ pruebabb.py
@@ -12,47 +12,131 @@ import json
 from streamlit_lottie import st_lottie
 
 
+#Layout
+st.set_page_config(
+    page_title="Practica 1",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+#Data Pull and Functions
+st.markdown("""
+<style>
+.big-font {
+    font-size:80px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+@st.cache_data
+def load_lottiefile(filepath: str):
+    with open(filepath,"r") as f:
+        return json.load(f)
+
+@st.cache_data
+def pull_clean():
+    master_zip=pd.read_csv('ConteoDeCarasPorParejas.csv',dtype={'ZCTA5': str})
+    return master_zip
 
 
-# Función para leer y mostrar el contenido del archivo Practica1.py
+
+#Options Menu
+with st.sidebar:
+    selected = option_menu('Menu', ["Intro", 'Graficas','Info', 'Distribucion binomial'], 
+        icons=['comment-alt','stats','info-circle'],menu_icon='intersect', default_index=0)
+    lottie = load_lottiefile("digglet.json")
+    st_lottie(lottie,key='loc')
+
+#Intro Page
+if selected=="Intro":
+    #Header
+    st.title('Distribución Binomial')
+    st.subheader('*asdasdassssssssssssssssssssssssssssssssssss.*')
+
+    st.divider()
+
+    #Use Cases
+    with st.container():
+        col1,col2=st.columns(2)
+        with col1:
+            st.header('Resumen')
+            st.markdown(
+                """
+                asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+                """
+                )
+            st.header('Objetivos')
+            st.markdown(
+                """
+                asdddddddddddddddddddd
+                """
+                )
+            
+        with col2:
+            st.header('Marco teorico')
+            st.markdown(
+                """
+                asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                """
+                )
+            st.header('Conclusiones')
+            st.markdown(
+                """
+                asdddddddddddddddddddd
+                """)
+
+    
+
+
+    
+#Search Page
+if selected=='Graficas':
+    # Set up the Streamlit ap
+
+ # Add a title to the app
+ st.title("CSV File Viewer")
+
+# Add a file uploader to the app
+ 
+
+ df = pd.read_csv("ConteosDeCarasPorPareja.csv")
+  # Display the DataFrame in Streamlit
+ st.dataframe(df)
+
+   # Display some statistics about the DataFrame
+ st.write(f"The DataFrame has {len(df)} rows and {len(df.columns)} columns.")
+
+
+
+#---------------------------------------------------------------
+# Grafica
+if selected=='Distribucion binomial':
+
 def leer_archivo_py():
     try:
         with open("Practica1.py", "r") as archivo:
             contenido = archivo.read()
-            st.text_area("Contenido del archivo Practica1.py", contenido)
+            st.text_area("Contenido del archivo", contenido)
     except FileNotFoundError:
         st.error("El archivo no fue encontrado.")
 
-# Función principal de la aplicación Streamlit
 def main():
-    st.set_page_config(
-        page_title="Practica 1",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
+    st.title("aaaaa")
+    st.write("sdasdasdasd.")
 
-    # Opciones del menú en la barra lateral
-    with st.sidebar:
-        selected = st.radio('Menú', ["Intro", 'Gráficas', 'Distribución binomial', 'Info'], index=0)
+    leer_archivo_py()
 
-    # Páginas de la aplicación
-    if selected == "Intro":
-        st.title('Distribución Binomial')
-        st.subheader('*asdasdassssssssssssssssssssssssssssssssssss.*')
-        st.divider()
-        # Aquí puedes agregar el contenido de la página de introducción
-    elif selected == 'Gráficas':
-        st.title("CSV File Viewer")
-        df = pd.read_csv("ConteosDeCarasPorPareja.csv")
-        st.dataframe(df)
-        st.write(f"El DataFrame tiene {len(df)} filas y {len(df.columns)} columnas.")
-    elif selected == 'Distribución binomial':
-        leer_archivo_py()
-    elif selected == 'Info': 
-        st.title('Datos')
-        st.markdown("""
-            asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        """)
+#-----------------------------------------------------------------
+    
+  
+
+ 
+#About Page
+if selected=='Info': 
+    st.title('Datos')
+    st.markdown(
+                """
+                asdasdasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                """)
 
 if __name__ == "__main__":
     main()
