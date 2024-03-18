f7b60fc757cf9bafb1448e0e461b02c46660136b
JGuillermoMM <g.monterroso.m@gmail.com>
2024-03-17 23:11:07 -0600

    trabajoTerminado

--- Laboratorio1_LabRedDat.py
+++ Laboratorio1_LabRedDat.py
@@ -15,7 +15,7 @@ from streamlit_lottie import st_lottie
 st.set_option('deprecation.showPyplotGlobalUse', False)
 from pydub import AudioSegment
 from pydub.playback import play
-
+import scipy.stats as ss
 
 
 
@@ -80,7 +80,7 @@ if selected=="Intro":
     margin-top: 5px;
  }
   </style>
- <div class="centered-italic">This is some centered and italicized content.</div>
+ <div class="centered-italic">José Guillermo Monterroso Marroquín, 202005689 y Shawn César Alejandro García Fernández, 201906567 </div>
  """, unsafe_allow_html=True)
 
  st.divider()
@@ -125,10 +125,38 @@ if selected=="Intro":
             st.subheader('Específicos')
             st.markdown(
                 """
-                - Verificar le toeria de la distribución binomial
-                - Verificar que los datos obtenidos experimentalmente sigan un comportamiento ¿binomial? xd
+                - Verificar la teoria de la distribución binomial
+                - Verificar que los datos obtenidos experimentalmente sigan un comportamiento de distribución binomial
                 """
                 )
+            st.markdown("""
+              <style>
+             .big-title {
+             text-align: center;
+              font-size: 36px;
+               margin-top: 5px;
+               }
+              </style>
+             <div class="big-title">Marco teorico</div>
+            """, unsafe_allow_html=True)
+            st.subheader('Coeficiente Binomial')
+            st.markdown(
+                """
+                El coeficiente binomial indica el numero de subconjuntos 
+                de k elementos escogidos de un conjunto de n elementos totales. Puede ser calculado de la siguiene manera
+                """
+                )
+            st.latex(r'''\binom{n}{k}= \frac{n!}{k!(n-k)!}''')
+            st.subheader('Distribución Binomial')
+            st.markdown("La distribución Binomial, llamada así por el coeficiente binomial, permite describir la probabilidad de observar una cantidad de éxitos en una cantidad de intentos, considerando la probabilidad individual de éxito de cada intento. Específicamente, estas características se dividen en las siguientes variables: ")
+            st.markdown(""" 
+                        - Número de ensayos (n): Representa el número total de ensayos o experimentos. 
+                        - Probabilidad de éxito (p): Es la probabilidad de que ocurra un éxito en un solo ensayo. 
+                        - Número de éxitos (x): Es la variable discreta que representa el número de éxitos que se observan en los n ensayos.
+                        """)
+            st.latex(r'''P(X = k) = \binom{n}{k} p^k (1-p)^{n-k}''')
+
+
             st.title('Reproductor de Spotify')
 
 # Enlace a la canción de Spotify
@@ -142,33 +170,6 @@ if selected=="Intro":
 
       #Columna dos de introducción       
         with col2:
-             st.markdown("""
-              <style>
-             .big-title {
-             text-align: center;
-              font-size: 36px;
-               margin-top: 5px;
-               }
-              </style>
-             <div class="big-title">Marco teorico</div>
-            """, unsafe_allow_html=True)
-             st.subheader('Coeficiente Binomial')
-             st.markdown(
-                """
-                El coeficiente binomial indica el numero de subconjuntos 
-                de k elementos escogidos de un conjunto de n elementos totales. Puede ser calculado de la siguiene manera
-                """
-                )
-             st.latex(r'''\binom{n}{k}= \frac{n!}{k!(n-k)!}''')
-             st.subheader('Distribución Binomial')
-             st.markdown("La distribución Binomial, llamada así por el coeficiente binomial, permite describir la probabilidad de observar una cantidad de éxitos en una cantidad de intentos, considerando la probabilidad individual de éxito de cada intento. Específicamente, estas características se dividen en las siguientes variables: ")
-             st.markdown(""" 
-                        - Número de ensayos (n): Representa el número total de ensayos o experimentos. 
-                        - Probabilidad de éxito (p): Es la probabilidad de que ocurra un éxito en un solo ensayo. 
-                        - Número de éxitos (x): Es la variable discreta que representa el número de éxitos que se observan en los n ensayos.
-                        """)
-             st.latex(r'''P(X = k) = \binom{n}{k} p^k (1-p)^{n-k}''')
-
              st.markdown("""
               <style>
              .big-title {
@@ -186,6 +187,46 @@ if selected=="Intro":
                 cómo evoluciona esta distribución conforme se incrementa la cantidad de lanzamientos verificando
                 que la teoria de la distribución binomial es consistente con los resultado obtenidos esperimentalmente.
                 """)
+             st.markdown("""
+              <style>
+             .big-title {
+              text-align: center;
+             font-size: 36px;
+             margin-top: 5px;
+               }
+             </style>
+             <div class="big-title">Discusión de Resultados</div>""", unsafe_allow_html=True)
+             st.markdown(
+                """
+                - A medida que el valor de m aumenta, la distribución adquiera mayor simetría y se asemeje más a una forma de campana, la cual es típica de la distribución binomial.
+                - Aunque se consideraron los eventos de todas las parejas, no se pudieron obtener todos los casos.. Sin embargo, las gráficas siempre mostraron una tendencia hacia la media, lo cual es consistente con el comportamiento esperado de una distribución binomial en este escenario de tiros de moneda.
+                """)
+             
+             
+
+
+
+
+             st.markdown("""
+  <style>
+  .big-title {
+    text-align: center;
+    font-size: 36px;
+    margin-top: 5px;
+ }
+ </style>
+ <div class="big-title">Referencias</div>
+ """, unsafe_allow_html=True)
+             st.markdown(
+                """
+                - Taylor, John R. “An introduction to error analysis, The study of uncertainties in physical measure-
+                ments”. Second edition. University science books. 1982. 
+                - https://www.linode.com/docs/guides/how-to-choose-python-api-framework/
+                - https://es.wikipedia.org/wiki/Coeficiente_binomial
+                - https://tecnonovax.wordpress.com/reproducir-musica-en-spotify-con-python/
+                
+                """
+                )
 
     
 
@@ -198,71 +239,76 @@ if selected=='Graficas':
     st.write(f"The DataFrame has {len(df)} rows and {len(df.columns)} columns.")
     
 # Datos de las caras de las monedas
-listas = {
+    # Datos de las caras de las monedas
+    listas = {
     "Guillermo y Shawn": [2, 4, 5, 3, 7, 3, 4, 6, 4, 4, 3, 5, 3, 2, 3, 4, 8, 6, 4, 2, 5, 5, 3, 8, 4, 7, 4, 6, 3, 5, 8, 7, 3, 3, 6, 5, 4, 4, 5, 2, 5, 3, 7, 6, 3, 6, 5, 2, 4, 6, 5, 4, 6, 3, 6, 5, 3, 7, 8, 7, 4, 4, 4, 8, 5, 4, 3, 5, 7, 5, 2, 2, 3, 5, 1, 6, 4, 6, 4, 4, 3, 3, 6, 6, 3, 4, 5, 5, 5, 7, 6, 7, 4, 3, 5, 4, 5, 7, 6, 5],
     "Lobsang y Rebeca": [6, 5, 5, 6, 5, 4, 6, 6, 5, 4, 6, 6, 5, 6, 9, 8, 1, 7, 5, 3, 5, 3, 3, 4, 3, 5, 4, 4, 6, 2, 5, 6, 7, 5, 5, 2, 3, 5, 7, 6, 5, 1, 6, 4, 4, 8, 5, 3, 6, 5, 6, 4, 5, 5, 3, 2, 6, 5, 2, 9, 7, 4, 7, 4, 3, 3, 6, 6, 4, 4, 6, 5, 5, 4, 6, 4, 9, 6, 4, 4, 8, 6, 4, 4, 8, 6, 8, 3, 6, 2, 5, 6, 2, 4, 5, 3, 4, 6, 5, 7],
     "Diego y Saul": [5, 6, 5, 5, 4, 4, 4, 3, 6, 5, 4, 7, 5, 7, 3, 5, 4, 7, 3, 4, 6, 3, 4, 5, 6, 2, 7, 3, 6, 2, 4, 7, 5, 5, 5, 3, 6, 6, 5, 4, 4, 7, 4, 7, 6, 5, 4, 4, 3, 5, 5, 4, 4, 7, 4, 5, 5, 4, 7, 6, 9, 5, 5, 5, 4, 5, 5, 7, 5, 4, 8, 3, 4, 4, 4, 8, 4, 9, 7, 7, 5, 5, 7, 5, 4, 4, 6, 7, 4, 2, 5, 5, 3, 6, 7, 5, 4, 4, 4, 7],
     "Giovanna y Mario": [5, 5, 5, 7, 7, 4, 7, 4, 6, 6, 6, 5, 5, 5, 6, 6, 6, 6, 7, 4, 5, 4, 5, 6, 5, 8, 7, 4, 3, 6, 4, 3, 6, 2, 7, 5, 8, 7, 6, 7, 4, 5, 5, 6, 4, 7, 4, 6, 4, 3, 4, 5, 5, 4, 3, 5, 6, 7, 5, 4, 5, 4, 4, 4, 6, 8, 6, 7, 5, 1, 3, 6, 4, 5, 4, 3, 5, 4, 3, 4, 6, 8, 5, 6, 5, 7, 5, 4, 6, 5, 4, 4, 10, 8, 3, 7, 5, 5, 4, 4],
     "Dessiré y Fabricio": [6, 4, 3, 6, 6, 6, 6, 7, 4, 4, 5, 4, 5, 3, 4, 8, 5, 3, 6, 6, 6, 5, 5, 5, 6, 4, 6, 6, 7, 6, 6, 6, 5, 4, 2, 5, 3, 6, 4, 4, 6, 5, 3, 4, 5, 5, 6, 5, 7, 5, 3, 3, 5, 5, 5, 4, 10, 5, 6, 4, 3, 5, 6, 4, 3, 4, 6, 5, 4, 6, 8, 5, 5, 5, 4, 5, 8, 4, 5, 5, 3, 3, 4, 5, 2, 7, 4, 5, 4, 6, 5, 6, 3, 6, 5, 7, 7, 9, 5, 3],
     "Jacobo y Cesar": [6, 6, 5, 3, 2, 5, 7, 8, 4, 5, 3, 4, 7, 6, 8, 4, 2, 3, 7, 2, 7, 6, 2, 5, 8, 2, 4, 4, 5, 5, 3, 6, 3, 5, 6, 6, 3, 6, 7, 3, 5, 4, 5, 4, 3, 5, 6, 4, 7, 4, 7, 6, 4, 6, 7, 6, 7, 4, 2, 4, 3, 4, 5, 5, 7, 4, 5, 4, 2, 4, 7, 5, 3, 5, 5, 4, 4, 6, 5, 4, 4, 4, 5, 4, 6, 6, 6, 8, 3, 5, 7, 3, 4, 8, 4, 6, 5, 4, 6, 4]
-}
+            }
 
 # Función para plotear el histograma y el ajuste
-def plot_histogram_and_fit(data, m, hist_color, fit_color, mean_color, std_dev_color):
+    def plot_histogram_and_fit(data, m, hist_color, fit_color, mean_color, std_dev_color):
     # Obtener los datos
-    data_selected = data[:m]
+        data_selected = data[:m]
     
     # para la grafica y sus colores
-    mean = np.mean(data_selected)
-    std_dev = np.std(data_selected)
+        mean = np.mean(data_selected)
+        std_dev = np.std(data_selected)
     
-    # Para el histograma
-    plt.hist(data_selected, bins=np.arange(min(data_selected), max(data_selected)+1)-0.5, density=True, alpha=0.6, color=hist_color, edgecolor='black', linewidth=1.2, label='Datos experimentales')
+    # Calcular p
+        n = len(data_selected)
+        p = mean / n
     
     # Fit de la distribución binomial
-    x = np.arange(0, max(data_selected)+1)
-    n = len(x)
-    p = mean / n
-    y = binom.pmf(x, n, p)
-    
-    # Para el ajuste
-    plt.plot(x, y, 'r--', linewidth=1.5, label=f'Ajuste Binomial\nMedia: {mean:.2f}\nDesviación Estándar: {std_dev:.2f}')
-    
-    # Graficar la media y la desviación estándar
-    plt.axvline(x=min(data_selected), color=mean_color, linestyle='-', linewidth=2, label=f'Valor mínimo: {min(data_selected)}')
-    plt.axvline(x=mean, color=std_dev_color, linestyle='-', linewidth=2, label=f'Desviación estándar: {std_dev:.2f}')
-
-    plt.xlabel('Número de Caras')
-    plt.ylabel('Densidad de probabilidad')
-    plt.title(f'Histograma y Ajuste Binomial para los primeros {m} tiros del conjunto de datos')
-    plt.legend()
-    plt.grid(True)
-
-    # Ajustar la posición del cuadro de texto
-    plt.tight_layout()
-
-    st.pyplot()
+        x = np.arange(0, max(data_selected)+1)
+        y = binom.pmf(x, n, p)
+        return data_selected, mean, std_dev, p, y
 
 # Crear la interfaz de usuario 
-def main():
-    st.title('Ajuste Binomial y Histograma Interactivo')
-    
-    dataset = st.selectbox('Selecciona un conjunto de datos:', ["Clase"] + list(listas.keys()))
-    
-    if dataset == "Clase":
-        data_selected = [item for sublist in listas.values() for item in sublist]
-        m = 500
-    else:
-        data_selected = listas[dataset]
-        m = st.slider('Selecciona el valor de m:', 1, 100, 10)
-        
-    hist_color = st.color_picker('Color del histograma:', '#00f')
-    fit_color = st.color_picker('Color del ajuste:', '#f00')
-    mean_color = st.color_picker('Color del valor mínimo:', '#0f0')
-    std_dev_color = st.color_picker('Color de la desviación estándar:', '#ffa500')
+    def main():
+        st.title('Ajuste Binomial y Histograma Interactivo')
+        dataset = st.selectbox('Selecciona un conjunto de datos:', ["Clase"] + list(listas.keys()))
+        if dataset == "Clase":
+            data_selected = [item for sublist in listas.values() for item in sublist]
+            m = 500
+        else:
+            data_selected = listas[dataset]
+            m = st.slider('Selecciona el valor de m:', 1, 100, 10)
+        hist_color = st.color_picker('Color del histograma:', '#00f')
+        fit_color = st.color_picker('Color del ajuste:', '#f00')
+        mean_color = st.color_picker('Color del valor mínimo:', '#0f0')
+        std_dev_color = st.color_picker('Color de la desviación estándar:', '#ffa500')
+        data, mean, std_dev, p, y = plot_histogram_and_fit(data_selected, m, hist_color, fit_color, mean_color, std_dev_color)
+        st.write('---')
+        st.header('Valores obtenidos del ajuste y medidos experimentalmente:')
+        st.write(f'Conteo medio de caras (ajuste binomial): {mean:.2f}')
+        st.write(f'Desviación estándar (ajuste binomial): {std_dev:.2f}')
+        st.write(f'Valor de p (ajuste binomial): {p:.2f}')
+        st.write(f'Conteo medio de caras (experimental): {np.mean(data):.2f}')
+        st.write('---')
 
-    plot_histogram_and_fit(data_selected, m, hist_color, fit_color, mean_color, std_dev_color)
- 
+    # Para el histograma
+        plt.hist(data, bins=np.arange(min(data), max(data)+1)-0.5, density=True, alpha=0.6, color=hist_color, edgecolor='black', linewidth=1.2, label='Datos experimentales')
+    
+    # Para el ajuste
+        plt.plot(np.arange(len(y)), y, 'r--', linewidth=1.5, label=f'Ajuste Binomial\nMedia: {mean:.2f}\nDesviación Estándar: {std_dev:.2f}')
+    
+    # Graficar la media y la desviación estándar
+        plt.axvline(x=min(data), color=mean_color, linestyle='-', linewidth=2, label=f'Valor mínimo: {min(data)}')
+        plt.axvline(x=mean, color=std_dev_color, linestyle='-', linewidth=2, label=f'Desviación estándar: {std_dev:.2f}')
 
-if __name__ == '__main__':
-    main() 
+    #plt.xlabel('Número de Caras')
+    #plt.ylabel('Densidad de probabilidad')
+    #plt.title(f'Histograma y Ajuste Binomial para los primeros {m} tiros del conjunto de datos')
+        plt.legend()
+        plt.grid(True)
+
+    # Ajustar la posición del cuadro de texto
+        plt.tight_layout()
+        st.pyplot()
+
+    if __name__ == '__main__':
+        main()
